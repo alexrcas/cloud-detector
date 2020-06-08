520e25797772a5a7b8038a495b1e7f8b3f1476e9
Alexis Rodríguez Casañas <alu0100711837@ull.edu.es>
2020-06-08 19:06:54 +0100

    Update local.js

--- static/local.js
+++ static/local.js
@@ -1,4 +1,3 @@
-//Get camera video
 const constraints = {
     audio: false,
     video: {
@@ -10,8 +9,6 @@ const constraints = {
 navigator.mediaDevices.getUserMedia(constraints)
     .then(stream => {
         document.getElementById("myVideo").srcObject = stream;
-        console.log("Got local user video");
-
     })
     .catch(err => {
         console.log('navigator.getUserMedia error: ', err)
